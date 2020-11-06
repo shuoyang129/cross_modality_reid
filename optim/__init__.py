9627c55d17d3e4c6765b1b70e14c6dabdd0439aa
Shuo Yang <yangshuo129@gmail.com>
2020-11-06 16:00:44 +0800

    fix

--- optim/__init__.py
+++ optim/__init__.py
@@ -3,18 +3,25 @@
 @contact:   guan.wang0706@gmail.com
 """
 
-from .lr_scheduler import WarmupMultiStepLR, DelayedCosineAnnealingLR, WarmupCosineAnnealingLR
+from .lr_scheduler import (
+    WarmupMultiStepLR,
+    DelayedCosineAnnealingLR,
+    WarmupCosineAnnealingLR,
+    CustomMultiStepLR,
+)
 
 
 class Optimizer(object):
 
-    KWARGS = ['fix_cnn_epochs']
+    KWARGS = ["fix_cnn_epochs"]
 
     def __init__(self, optimizer, lr_scheduler, max_epochs, **kwargs):
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
         self.max_epochs = max_epochs
         for key, value in kwargs.items():
-            assert key in Optimizer.KWARGS, 'expect {}, but got {}'.format(Optimizer.KWARGS, key)
+            assert key in Optimizer.KWARGS, "expect {}, but got {}".format(
+                Optimizer.KWARGS, key
+            )
             setattr(self, key, value)
 
